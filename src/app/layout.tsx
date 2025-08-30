754145152b22d3aa23e73a552d8d934b2a74ce35
Yogi Prasetya <hello@yogiprasetya.com>
2025-08-30 15:47:00 +0700

    default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <NextAuthProvider>
             <SWRProvider>
               <div className="min-h-screen bg-background">{children}</div>
